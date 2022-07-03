8fcab5e6ebaf4128f0cd0a7e49ca64c74a5adccc
Mykhailo Shyhin <johnglagol@gmail.com>
2022-07-03 17:02:46 +0300

    done task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtnColor = document.querySelector('[data-start]');
+const stopBtnColor = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtnColor.addEventListener('click', startChangeBackgroundColor);
+
+stopBtnColor.addEventListener('click', stopChangeBackgroundColor);
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function startChangeBackgroundColor() {
+    timerId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnColor.setAttribute('disabled', '');
+};
+
+function stopChangeBackgroundColor() {
+  clearInterval(timerId);
+  startBtnColor.removeAttribute('disabled');
+};
