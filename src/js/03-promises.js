e04ce466c6d0367766ed3490ff1599f05b544004
Mykhailo Shyhin <johnglagol@gmail.com>
2022-07-05 16:47:30 +0300

    done task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,31 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
-let FIRST_DELAY;
-let DELAY_STEP;
-let AMOUNT;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  FIRST_DELAY = form.delay.value;
-  DELAY_STEP = form.step.value;
-  AMOUNT = form.amount.value;
-  form.reset()
-  
-  console.log(FIRST_DELAY)
+
+  let DELAY = Number(form.delay.value);
+
+  for (let i = 1; i <= Number(form.amount.value); i += 1) {
+    createPromise(i , DELAY).then(result => Notiflix.Notify.success(result)).catch(error => Notiflix.Notify.failure(error));
+    
+    DELAY += Number(form.step.value);
+  }
 
 })
 
- Notiflix.Notify.failure('Please choose a date in the future');
-
 
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    setTimeout(() => {
+      if (shouldResolve) {
+      resolve(`Fulfilled promise ${position} in ${delay}ms`);
+   } else {
+      reject(`Rejected promise ${position} in ${delay}ms`);
+   }
+  }, delay)})
+  
 };
 
